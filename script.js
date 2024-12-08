d27a356f5ecc5528fb9e882389a28121ddc83c21
BigCheungus <Cheungusb@gmail.com>
2024-12-08 18:18:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".copy").forEach((copyButton) => {
+  copyButton.addEventListener("click", () => {
+    const targetElement = document.querySelector(copyButton.dataset.copy);
+    const textToCopy = targetElement.textContent.replace(/\s+/g, "").trim();
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      copyButton.disabled = true;
+    });
+  });
+});
